d379cf6ed2d2805cf686be82a7c851f7667959e9
staticshock7 <murra275@purdue.edu>
2025-07-18 08:53:16 -0500

    Update app.py
    - removed port config form
    - removed input for lb, fw, rtr ports
    + included logic for exiting script from webGUI

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
-# import your cable1 function as needed
-from cableTest_v2 import cable1
+from cable_5 import main
 import webbrowser
+from os import getpid, kill
+from signal import SIGINT
 from time import sleep
 
 app = Flask(__name__)
@@ -10,11 +11,10 @@ app = Flask(__name__)
 def index():
     
     if request.method == "POST":
-        # Print all form data for debugging
+
         print(request.form)
 
         device_input = request.form.get("device_input", "").strip()
-        port_config = request.form.get("port_config", "").strip()
         username = request.form.get("username", "").strip()
         ssh_pass1 = request.form.get("ssh_pass1", "")
         ssh_pass2 = request.form.get("ssh_pass2", "")
@@ -22,34 +22,31 @@ def index():
         if not device_input:
             return "Error: You must enter the device list or file path."
 
-        # Determine ports
-        if port_config == "standard":
-            lb_port, rtr_lb_port, rtr_fw_port = "XX", "1/XX-XX", "1/XX-XX"
-        else:
-            lb_port = request.form.get("lb_port", "").strip()
-            rtr_lb_port = request.form.get("rtr_lb_port", "").strip()
-            rtr_fw_port = request.form.get("rtr_fw_port", "").strip()
-
-        # Call your function (you may need to refactor cable1 to accept these args)
-        lb1, fw1, lb2, fw2 = cable1(
+        lb_even, lb_odd, fw_even, fw_odd = main(
             key1=ssh_pass1,
             key2=ssh_pass2,
             user=username,
             device_input=device_input,
-            lb_port=lb_port,
-            rtr_lb_port=rtr_lb_port,
-            rtr_fw_port=rtr_fw_port
         )
 
         return render_template("results.html",
-                               lb_results=lb1 + lb2,
-                               fw_results=fw1 + fw2)
+                               lb_results = lb_even + lb_odd,
+                               fw_results = fw_even + fw_odd)
 
     return render_template("index.html")
 
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    def delayed_shutdown():
+        import time
+        sleep(1)
+        kill(getpid(), SIGINT)
+
+    import threading
+    threading.Thread(target=delayed_shutdown).start()
+    return 'Shutting down...'
 
 if __name__ == "__main__":
     # Launch Chrome at localhost
     webbrowser.get(using='windows-default').open_new("http://127.0.0.1:5000")
-    app.run(debug=True)
-
+    app.run(debug=True, use_reloader=False)
